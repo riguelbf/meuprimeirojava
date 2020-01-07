8113914f74b7050a55b0c134e1899d2c987e4474
Your Name <you@example.com>
2020-01-07 20:30:58 -0300

    primeiro commit

---
+++ src/main/java/com/cursojava/rigueljava/RigueljavaApplication.java
@@ -0,0 +1,13 @@
+package com.cursojava.rigueljava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RigueljavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RigueljavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursojava/rigueljava/RigueljavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursojava.rigueljava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RigueljavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
